b7234fbbecd190d98fb572c21104cf063c8b892c
yan0kom <yan0kom@gmail.com>
2023-04-09 18:28:21 +0300

    added test of poker hand sorting

--- src/test/java/tt/holdem/game/PokerHandTest.java
+++ src/test/java/tt/holdem/game/PokerHandTest.java
@@ -2,8 +2,10 @@ package tt.holdem.game;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class PokerHandTest {
     @Test
@@ -40,5 +42,64 @@ class PokerHandTest {
         assertEquals(8, new PokerHand("3H 3D KS 3S 3C").getCombination().getCombinationValue());
         assertEquals(9, new PokerHand("7C TC 9C 8C 6C").getCombination().getCombinationValue());
         assertEquals(10, new PokerHand("KS TS QS JS AS").getCombination().getCombinationValue());
+    }
+
+    @Test
+    void shouldSortHandsByRank() {
+        var hands = new ArrayList<PokerHand>();
+        // royal flush
+        hands.add(new PokerHand("KS AS TS JS QS"));
+        // straight flush
+        hands.add(new PokerHand("TH JH 9H 7H 8H"));
+        hands.add(new PokerHand("TC 6C 9C 7C 8C"));
+        hands.add(new PokerHand("5C 6C 9C 7C 8C"));
+        // quads
+        hands.add(new PokerHand("AS TC TS TD TH"));
+        hands.add(new PokerHand("AS 9C 9S 9D 9H"));
+        hands.add(new PokerHand("2S 9C 9S 9D 9H"));
+        // full house
+        hands.add(new PokerHand("TH 9C 9D TS 9H"));
+        hands.add(new PokerHand("QH QC 7D 7S 7H"));
+        hands.add(new PokerHand("TH 7C 7D TS 7H"));
+        // flush
+        hands.add(new PokerHand("QS 5S JS 9S TS"));
+        hands.add(new PokerHand("QS 8S 2S 9S TS"));
+        hands.add(new PokerHand("QS 8S 2S 7S TS"));
+        hands.add(new PokerHand("QS 8S 3S 5S TS"));
+        hands.add(new PokerHand("QS 8S 2S 5S TS"));
+        hands.add(new PokerHand("TC 4C 9C 7C 8C"));
+        // straight
+        hands.add(new PokerHand("QC 8S JC 9C TH"));
+        hands.add(new PokerHand("TC 6H 9C 7C 8H"));
+        hands.add(new PokerHand("2C 3S 6C 4C 5H"));
+        // triple
+        hands.add(new PokerHand("AD TC TS 5C TH"));
+        hands.add(new PokerHand("KD TC TS QC TH"));
+        hands.add(new PokerHand("KD TC TS 9C TH"));
+        hands.add(new PokerHand("AD 3C 5H 3S 3H"));
+        // two pairs
+        hands.add(new PokerHand("AS AC TS 5C TH"));
+        hands.add(new PokerHand("JS JC TS 5C TH"));
+        hands.add(new PokerHand("JS JC 8S KC 8H"));
+        hands.add(new PokerHand("JS JC 8S 5C 8H"));
+        // pairs
+        hands.add(new PokerHand("5S JH KD JS TC"));
+        hands.add(new PokerHand("4S JH KD JS TC"));
+        hands.add(new PokerHand("5S JH KD JS 7C"));
+        hands.add(new PokerHand("5S JH QD JS 7C"));
+        hands.add(new PokerHand("5S 7H KD JS 7C"));
+        hands.add(new PokerHand("5S 7H KD JS 5C"));
+        // high card
+        hands.add(new PokerHand("5S 9H AD JS 7C"));
+        hands.add(new PokerHand("5S 7H KH JS 6C"));
+        hands.add(new PokerHand("3D 7H KD JS 6C"));
+        hands.add(new PokerHand("2D 7H KD JS 3C"));
+        hands.add(new PokerHand("5D 3H KD JS 6C"));
+        hands.add(new PokerHand("5S 6S KD TS 7D"));
+
+        var shuffled = new ArrayList<PokerHand>(hands);
+        Collections.shuffle(shuffled);
+        Collections.sort(shuffled);
+        assertIterableEquals(hands, shuffled);
     }
 }
