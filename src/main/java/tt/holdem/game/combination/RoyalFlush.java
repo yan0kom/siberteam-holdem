6288eee5b2f4a7bf58cf31efdce9ac55180b7875
yan0kom <yan0kom@gmail.com>
2023-04-09 13:46:47 +0300

    Royal flush implemented

---
+++ src/main/java/tt/holdem/game/combination/RoyalFlush.java
@@ -0,0 +1,56 @@
+package tt.holdem.game.combination;
+
+import tt.holdem.game.Card;
+import tt.holdem.game.CardRank;
+import tt.holdem.game.CardSuit;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+class RoyalFlush extends PokerCombination {
+    private static final Integer ROYAL_FLUSH_VALUE = 10;
+    public static Optional<PokerCombination> create(Card[] cards) {
+        assertCards(cards);
+        if (Arrays.stream(cards).map(Card::getSuit).distinct().count() != 1L) {
+            return Optional.empty();
+        }
+
+        var straightFlushOpt = StraightFlush.create(cards);
+        if (straightFlushOpt.isEmpty()) {
+            return Optional.empty();
+        }
+
+        var straightFlush = (StraightFlush) straightFlushOpt.get();
+        if (straightFlush.getRank() != CardRank.ACE) {
+            return Optional.empty();
+        }
+
+        var royalFlush = new RoyalFlush();
+        royalFlush.suit = straightFlush.getSuit();
+
+        return Optional.of(royalFlush);
+    }
+    static {
+        PokerCombinationRegistry.registerCombinationFactoryMethod(ROYAL_FLUSH_VALUE, RoyalFlush::create);
+    }
+
+    private CardSuit suit;
+
+    private RoyalFlush() {
+        super(ROYAL_FLUSH_VALUE);
+    }
+
+    protected int compareToSameTypeCombination(RoyalFlush other) {
+        return 0;
+    }
+
+    @Override
+    protected int compareToSameTypeCombination(PokerCombination other) {
+        return compareToSameTypeCombination((RoyalFlush) other);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Royal flush of %s", suit);
+    }
+}
---
+++ src/test/java/tt/holdem/game/combination/RoyalFlushTest.java
@@ -0,0 +1,33 @@
+package tt.holdem.game.combination;
+
+import org.junit.jupiter.api.Test;
+import tt.holdem.game.PokerHand;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RoyalFlushTest {
+
+    @Test
+    void create() {
+        var royalFlush = RoyalFlush.create(new PokerHand("KD AD TD JD QD").getCards());
+        var notRoyalFlush = RoyalFlush.create(new PokerHand("KD 9D TD JD QD").getCards());
+
+        assertTrue(royalFlush.isPresent());
+        assertTrue(notRoyalFlush.isEmpty());
+    }
+
+    @Test
+    void compareToSameTypeCombination() {
+        var _AdKdQdJdTd = RoyalFlush.create(new PokerHand("KD AD TD JD QD").getCards()).get();
+        var _AsKsQsJsTs = RoyalFlush.create(new PokerHand("KS AS TS JS QS").getCards()).get();
+
+        assertTrue(_AdKdQdJdTd.compareTo(_AsKsQsJsTs) == 0); // royal flush have same rank
+    }
+
+    @Test
+    void testToString() {
+        var _AsKsQsJsTs = RoyalFlush.create(new PokerHand("KS AS TS JS QS").getCards()).get();
+        assertEquals("Royal flush of S", _AsKsQsJsTs.toString());
+    }
+}
